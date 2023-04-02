f19d4569049036bf0a6d663abb87f967bc101e38
NS <nazslogger@gmail.com>
2023-04-02 17:04:01 +0300

    Initial commit

---
+++ src/main/java/edu/com/vegosbackend/VegosBackEndApplication.java
@@ -0,0 +1,13 @@
+package edu.com.vegosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegosBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VegosBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/com/vegosbackend/VegosBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.com.vegosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VegosBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
